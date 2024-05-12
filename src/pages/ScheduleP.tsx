a1200384e20d7897e95927e688a535cd481224c5
HenriqueFreitas07 <henriquefreitasm87@gmail.com>
2024-05-12 18:15:25 +0100

    Added Markers to the map and Toast for futher implementation

--- src/pages/ScheduleP.tsx
+++ src/pages/ScheduleP.tsx
@@ -1,17 +1,16 @@
-import { IonButtons, IonContent, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonButton, IonInput, IonItem, IonDatetime, IonDatetimeButton, IonModal } from '@ionic/react';
-import Card from '../components/Card';
-import Button from '../components/Button';
-import Input from '../components/Input';
+
+import { IonButtons, IonContent, IonHeader, IonPage, IonToolbar, IonButton, IonInput, IonDatetime, IonDatetimeButton, IonModal } from '@ionic/react';
+
 import Icon from '../components/Icon';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import React, { useEffect, useRef, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { GoogleMap, Marker, MarkerClusterer } from '@react-google-maps/api';
+import React, { useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useJsApiLoader } from '@react-google-maps/api';
-import { IonModalCustomEvent, OverlayEventDetail } from '@ionic/core';
 import ScheduleList from './ScheduleLists';
-
+import data from '../data.json'
+import { getValue, setValue } from '../getSet'
+import { APIProvider, AdvancedMarker, AdvancedMarkerProps, Map, Pin } from '@vis.gl/react-google-maps';
+import { ToastInfo } from '../components/Alert';
 
 
 const ScheduleP: React.FC = () => {
@@ -50,52 +49,72 @@ const ScheduleP: React.FC = () => {
         <IonContent fullscreen>
 
 
-          {isLoaded && <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={14}
-            center={center}
-            options={mapOptions}
-          >
-            <div className='card'>
-              <div  >
-                <IonInput type="search" label="Origin: " class="custom"></IonInput>
-              </div>
-              <div>
-                <IonInput type="search" label="Destination: " class="custom"> </IonInput>
-              </div>
+          {isLoaded &&
+            <APIProvider apiKey="AIzaSyABN7IX_NnN3Io35DMphYiHmHg2NsHd7zQ" >
+              <Map
+                mapId={"fcdc1a0f0ad84c5e"}
+                defaultCenter={center}
+                disableDefaultUI={true}
+                gestureHandling={"greedy"}
+                defaultZoom={13}
+              >
+                {/* markers */}
+                {
+                  getValue(data, "busStops").map((marker: any) => {
+                    return (
+                      <AdvancedMarker
+                        key={marker.id}
+                        position={marker.position}
+                        onClick={() => ToastInfo(marker.code, marker.name)}
 
-              {showFilters && (
-                <div className='container flex justify-left items-left ml-1.5 '>
-                  <IonDatetimeButton datetime="datetime"  ></IonDatetimeButton>
-                  <IonModal keepContentsMounted={true}>
-                    <IonDatetime id="datetime"
-                      formatOptions={{
-                        date: {
-                          weekday: 'short',
-                          month: 'long',
-                          day: '2-digit',
-                        },
-                        time: {
-                          hour: '2-digit',
-                          minute: '2-digit',
-                        },
-                      }}
-                    ></IonDatetime>
-                  </IonModal>
+                      >
+                        <Icon name="br-stop-circle" className='text-blue text-3xl' />
+                      </AdvancedMarker>
+                    )
+                  })
+                }
+
+                <div className='card'>
+                  <div>
+                    <IonInput type="search" label="Origin: " class="custom"></IonInput>
+                  </div>
+                  <div>
+                    <IonInput type="search" label="Destination: " class="custom"> </IonInput>
+                  </div>
+
+                  {showFilters && (
+                    <div className='container flex justify-left items-left ml-1.5 '>
+                      <IonDatetimeButton datetime="datetime"  ></IonDatetimeButton>
+                      <IonModal keepContentsMounted={true}>
+                        <IonDatetime id="datetime"
+                          formatOptions={{
+                            date: {
+                              weekday: 'short',
+                              month: 'long',
+                              day: '2-digit',
+                            },
+                            time: {
+                              hour: '2-digit',
+                              minute: '2-digit',
+                            },
+                          }}
+                        ></IonDatetime>
+                      </IonModal>
+                    </div>
+                  )}
+                  <div className='container flex justify-between'>
+                    <div className='flex buttonLeft items-center'>
+                      <button className=" flex flex-row content-center" onClick={() => setShowFilters(!showFilters)} >{!showFilters &&
+                        <Icon name="br-square-plus" className='inline-block mr-2 mt-0.5'  ></Icon>}
+                        {showFilters &&
+                          <Icon name="br-square-minus" className='inline-block mr-2 mt-0.5'  ></Icon>}
+                        Filters</button>
+                    </div>
+                    <button className="buttonRight self-center" onClick={(e) => { setIsOpen(true); }} ><Icon name="br-search-location" className='aspect-square inline-block mt-1'></Icon></button>
+                  </div>
                 </div>
-              )}
-              <div className='container flex justify-between'>
-                <div className='flex buttonLeft items-center'>
-                  <button className=" flex flex-row content-center" onClick={() => setShowFilters(!showFilters)} >{!showFilters &&
-                    <Icon name="br-square-plus" className='inline-block mr-2 mt-0.5'  ></Icon>}
-                    {showFilters &&
-                      <Icon name="br-square-minus" className='inline-block mr-2 mt-0.5'  ></Icon>}
-                    Filters</button>
-                </div>
-                <button className="buttonRight self-center" onClick={(e) => { setIsOpen(true); }} ><Icon name="br-search-location" className='aspect-square inline-block mt-1'></Icon></button>
-              </div>
-            </div>
-          </GoogleMap>
+              </Map>
+            </APIProvider>
           }
           <IonModal isOpen={isOpen} ref={modal} >
             <IonHeader className='bg-blue'>
@@ -108,7 +127,7 @@ const ScheduleP: React.FC = () => {
               </IonToolbar>
             </IonHeader>
             <IonContent >
-              <ScheduleList close={()=> {modal.current?.dismiss(); setIsOpen(false)} } />
+              <ScheduleList close={() => { modal.current?.dismiss(); setIsOpen(false) }} />
             </IonContent>
           </IonModal>
 
